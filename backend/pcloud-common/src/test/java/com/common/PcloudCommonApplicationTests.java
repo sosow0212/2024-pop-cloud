3b95b69d59590164fb666e2a35a49eca194b07a6
sosow0212 <sosow0212@naver.com>
2024-07-31 17:27:08 +0900

    feat: common 모듈 생성

---
+++ backend/pcloud-common/src/test/java/com/common/PcloudCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcloudCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
