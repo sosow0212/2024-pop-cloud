36b0cefb52d7b7245c9fc308cc4856b39b7e79a9
eom-tae-in <eti0728@daum.net>
2024-08-21 09:26:39 +0900

    refactor: @Query를 제거하고 existsByPopupsIdAndMemberId를 쿼리 메서드로 변경

--- backend/pcloud-domain/src/main/java/com/domain/popups/infrastructure/LikedPopupsJpaRepository.java
+++ backend/pcloud-domain/src/main/java/com/domain/popups/infrastructure/LikedPopupsJpaRepository.java
@@ -2,13 +2,10 @@ package com.domain.popups.infrastructure;
 
 import com.domain.popups.domain.LikedPopups;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface LikedPopupsJpaRepository extends JpaRepository<LikedPopups, Long> {
 
-    @Query("SELECT CASE WHEN COUNT(lp) > 0 THEN true ELSE false END FROM LikedPopups lp WHERE lp.popupsId = :popupsId AND lp.memberId = :memberId")
-    boolean existsByPopupsIdAndMemberId(@Param("popupsId") Long popupsId, @Param("memberId") Long memberId);
+    boolean existsByPopupsIdAndMemberId(Long popupsId, Long memberId);
 
     void deleteByPopupsIdAndMemberId(Long popupsId, Long memberId);
 
