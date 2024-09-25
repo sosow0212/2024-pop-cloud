6640711643da725b51aaef301b599618e4871f0b
Han-wo <hansw98@naver.com>
2024-09-25 19:14:29 +0900

    feat: 서버환경 설정

---
+++ frontend/src/mocks/index.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+
+export async function initMsw() {
+  if (typeof window === "undefined") {
+    const { server } = await import("./server");
+    server.listen();
+  } else {
+    const { worker } = await import("./browser");
+    await worker.start();
+  }
+}
