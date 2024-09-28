d6e869d196847fba03332cb867cc312a68a32d22
HANSANGWOO <99471821+Han-wo@users.noreply.github.com>
2024-09-28 14:52:56 +0900

    feat: MSW 를 설정한다 (#61)
    * feat: MSW 기본세팅
    
    * chore: 린트설정 변경
    
    * fix: 린트 설정 수정
    
    * fix: 린트 설정에 맞게 수정
    
    * fix: 린트 설정에 맞게 수정
    
    * feat: MSW 기본설정
    
    * feat: MSW 및 express 가상 서버 설정

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,12 +1,13 @@
 import "./globals.css";
 
-import type { Metadata } from "next";
-import { useEffect } from "react";
+import { Metadata } from "next";
 
 import MobileHeader from "@/components/mobile-header";
 import MobileSizeWatcher from "@/components/mobile-size-watcher";
 import NavBar from "@/components/nav-bar";
 import ModalProvider from "@/provider/modal-provider";
+
+import { MSWComponent } from "../mocks/msw-Initializer";
 
 export const metadata: Metadata = {
   title: "POP CLOUD",
@@ -18,22 +19,10 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      const initMocks = async () => {
-        if (process.env.NEXT_PUBLIC_API_MOCKING === "enabled") {
-          const { worker } = await import("../mocks/browser");
-          await worker.start({ onUnhandledRequest: "bypass" });
-        }
-      };
-
-      initMocks();
-    }
-  }, []);
-
   return (
     <html lang="ko">
       <body>
+        <MSWComponent />
         <ModalProvider />
         <MobileSizeWatcher />
         <MobileHeader />
