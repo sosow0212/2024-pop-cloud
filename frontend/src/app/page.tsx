2c905232fac2b3fc9b23909e1eee9a6f36362a19
AdamSeungheonShin <ssh9809@naver.com>
2024-09-30 23:01:46 +0900

    chore: 컴파일 오류로 msw세팅 수정

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,29 +1,20 @@
-import PopupCard, { Popup } from "@/components/common/popup-card";
-
-const popupMock: Popup = {
-  id: 9,
-  ownerId: 9,
-  title:
-    "코뿔소와 함께하는 잠수교 러닝중 아이스크림이 얼마나 빨리 녹는지 테스트 하기",
-  description: "아메리카노: 빨리 잠들기 대회",
-  location: "강원도 태백시",
-  isParkingAvailable: true,
-  fee: 89000,
-  startDate: "2024-08-02T19:32:19.379718",
-  endDate: "2024-08-02T19:32:19.379718",
-  openTimes: "평일 09:00 ~ 18:00,\n주말 12:00 ~ 21:00\n",
-  latitude: 37.556725,
-  longitude: 126.9234952,
-  publicTag: "예술",
-};
+// const popupMock: Popup = {
+//   id: 9,
+//   ownerId: 9,
+//   title:
+//     "코뿔소와 함께하는 잠수교 러닝중 아이스크림이 얼마나 빨리 녹는지 테스트 하기",
+//   description: "아메리카노: 빨리 잠들기 대회",
+//   location: "강원도 태백시",
+//   isParkingAvailable: true,
+//   fee: 89000,
+//   startDate: "2024-08-02T19:32:19.379718",
+//   endDate: "2024-08-02T19:32:19.379718",
+//   openTimes: "평일 09:00 ~ 18:00,\n주말 12:00 ~ 21:00\n",
+//   latitude: 37.556725,
+//   longitude: 126.9234952,
+//   publicTag: "예술",
+// };
 
 export default function Home() {
-  return (
-    <>
-      <div>HOME</div>
-      <div className="flex justify-center">
-        <PopupCard popup={popupMock} />
-      </div>
-    </>
-  );
+  return <div>HOME</div>;
 }
