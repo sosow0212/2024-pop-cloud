d2c93ae5ccd1ac213c0e2e590a31df616b9ba5d1
Jay <63213487+sosow0212@users.noreply.github.com>
2024-10-01 12:12:18 +0900

    feat: Scheduler 중복 처리 작업을 제어하는 기능 추가 (#71)
    * chore: Redisson 추가
    Co-authored-by: eom-tae-in <eti0728@daum.net>
    
    * chore: Redisson 추가
    Co-authored-by: eom-tae-in <101633465+eom-tae-in@users.noreply.github.com>
    
    * chore: Redisson 추가
    
    Co-authored-by: eom-tae-in <eti0728@daum.net>
    
    * feat: Redisson Config 추가
    
    Co-authored-by: eom-tae-in <eti0728@daum.net>
    
    * feat: Redis 분산락 및 AOP 추가 (Redisson 기반) -> AOP Order 설정을 통해 추후에 생성되는 AOP 우선순위 선점에 대한 설정 추가 (Transactional 관련 facade X)
    
    Co-authored-by: eom-tae-in <eti0728@daum.net>
    
    * feat: Redis 멱등키 기반 BatchLock 추가 (이전 문제인 분산락에서 타임아웃 전에 문제를 해결하면 다른 스레드가 바로 작업을 가져간다. 동시성 이슈가 아니었던 것)
    
    Co-authored-by: eom-tae-in <eti0728@daum.net>
    
    * feat: Batch-lock 및 실패시 재시도 처리 로직 추가
    
    Co-authored-by: eom-tae-in <eti0728@daum.net>
    
    * refactor: 네이밍 변경 및 설명 추가
    
    Co-authored-by: eom-tae-in <eti0728@daum.net>
    
    * refactor: github actions 버전 변경
    
    Co-authored-by: eom-tae-in <eti0728@daum.net>
    
    ---------
    
    Co-authored-by: eom-tae-in <eti0728@daum.net>

--- backend/pcloud-batch/src/main/java/com/batch/job/show/exhibition/application/ExhibitionBatchPublisher.java
+++ backend/pcloud-batch/src/main/java/com/batch/job/show/exhibition/application/ExhibitionBatchPublisher.java
@@ -2,10 +2,12 @@ package com.batch.job.show.exhibition.application;
 
 import com.batch.annotation.BatchPublisher;
 import com.batch.job.show.exhibition.application.event.ClearedExhibitionIpEvent;
+import com.common.annotation.BatchJob;
 import com.common.config.event.Events;
-import java.time.LocalDateTime;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
+
+import java.time.LocalDateTime;
 
 @Slf4j
 @BatchPublisher
@@ -13,6 +15,7 @@ public class ExhibitionBatchPublisher {
 
     private static final String EVERY_FIVE_AM = "0 0 5 * * *";
 
+    @BatchJob(keyName = "clearExhibitionIpCache")
     @Scheduled(cron = EVERY_FIVE_AM)
     public void clearExhibitionIpCache() {
         Events.raise(new ClearedExhibitionIpEvent(LocalDateTime.now()));
