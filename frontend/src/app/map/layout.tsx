9511310b1d446c5ecab6be51002501b1850c39cd
위영진 <qser155@naver.com>
2024-10-15 11:16:44 +0900

    refactor: /map/layout.tsx main 태그 변경

--- frontend/src/app/map/layout.tsx
+++ frontend/src/app/map/layout.tsx
@@ -1,7 +1,7 @@
 export default function MapLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-col">
-      <main className="my-10 flex-1">{children}</main>
+      <div className="my-10 flex-1">{children}</div>
     </div>
   );
 }
