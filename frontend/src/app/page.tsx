2b74674e97c5ce07ab33bd41821b6adc55badb82
Adam Shin <137388618+AdamSeungheonShin@users.noreply.github.com>
2024-10-21 22:17:50 +0900

    Feat: 메인 기본 ui 작성한다 (#97)
    * chore: 컴파일 오류로 msw세팅 수정
    
    * feat: 메인페이지 배너 캐러셀 추가, 기본 레이아웃 구상
    
    * feat: 메인페이지 기본 레이아웃 구성
    
    * feat: 하단 CTA, Link 태그로 변경
    
    * feat: commit for update
    
    * feat: ui 마무리

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,23 +1,66 @@
+import Link from "next/link";
+
 import CarouselUI from "@/components/common/carousel";
 import Slide from "@/components/common/popup-slide";
-import POPUPS_MOCK from "@/components/common/popup-slide/mockData";
+import mockData from "@/components/common/popup-slide/mockData";
+
+const BANNER_MOCK = [
+  "/images/rhino.webp",
+  "/images/cat.webp",
+  "/images/dog.webp",
+];
+
+const sectionStyle = "my-30 flex flex-col max-w-1100 mx-auto";
+const headingStyle = "pl-20 text-24 font-extrabold";
+const cardContainerStyle = "flex justify-center";
+const bannerStyle = "h-180 w-full bg-blue-100 ";
 
 export default function Home() {
-  // todo 페칭코드 작성 후 삭제
-  const images = [
-    "/images/mock.jpeg",
-    "/images/mock2.jpeg",
-    "/images/mock3.jpeg",
-  ];
   return (
     <>
-      <div>
-        <CarouselUI data={images} />
-      </div>
-      {/* todo 사이드바 수정되는 대로 레이아웃 마진 조정.. */}
-      <div className="mx-auto h-500 overflow-hidden md:p-40 lg:w-1200">
-        <Slide popups={POPUPS_MOCK} />
-      </div>
+      <section>
+        <CarouselUI data={BANNER_MOCK} />
+      </section>
+      <section className="mx-auto flex max-w-1100 flex-col py-30">
+        <h2 className="pl-20 text-24 font-extrabold">🔥 인기 팝업</h2>
+        <div className={cardContainerStyle}>
+          <Slide popups={mockData} />
+        </div>
+      </section>
+      <section className={sectionStyle}>
+        <h2 className={headingStyle}>🌟 인기 전시</h2>
+        <div className={cardContainerStyle}>
+          <Slide popups={mockData} />
+        </div>
+      </section>
+      <div className={bannerStyle}>Banner</div>
+      <section className={sectionStyle}>
+        <h2 className={headingStyle}>Upcoming..</h2>
+        <div className={cardContainerStyle}>
+          <Slide popups={mockData} />
+        </div>
+      </section>
+      <div className={bannerStyle}>Banner</div>
+      <section className={sectionStyle}>
+        <h2 className={headingStyle}>추천 팝업</h2>
+        <div className={cardContainerStyle}>
+          <Slide popups={mockData} />
+        </div>
+      </section>
+      <section className={sectionStyle}>
+        <h2 className={headingStyle}>추천 전시</h2>
+        <div className={cardContainerStyle}>
+          <Slide popups={mockData} />
+        </div>
+      </section>
+      <div className={bannerStyle}>Banner</div>
+      {/* CTA */}
+      <section
+        className={`${sectionStyle} mb-100 flex flex-col gap-10 px-20 text-24 font-extrabold`}
+      >
+        <Link href="/">Showcase yours🌏</Link>
+        <Link href="/">Subscribe our newsletters!</Link>
+      </section>
     </>
   );
 }
