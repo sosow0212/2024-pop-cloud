9c7d83fa1b2dc12ab3c93776c3e86e303ad7e388
Han-wo <hansw98@naver.com>
2024-11-10 17:14:19 +0900

    feat: dev 모드에서 standalone 적용안되게 수정

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,7 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone",
+  output: process.env.NODE_ENV === "production" ? "standalone" : undefined,
   images: {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
