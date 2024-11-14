c7dda17ed9553b4661200800cc0221450f79e4f9
eom-tae-in <eti0728@daum.net>
2024-11-14 21:24:45 +0900

    feat: s3에 이미지 업로드하는 로직 추가
    Co-authored-by: sosow0212 <sosow0212@naver.com>

---
+++ backend/pcloud-api/src/main/java/com/api/global/config/S3Config.java
@@ -0,0 +1,30 @@
+package com.api.global.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+@Configuration
+public class S3Config {
+
+    @Value("${cloud.aws.credentials.accessKey}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secretKey}")
+    private String secretKey;
+
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
+    @Bean
+    public S3Client getS3Client() {
+        return S3Client.builder()
+                .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey, secretKey)))
+                .region(Region.of(region))
+                .build();
+    }
+}
--- backend/pcloud-api/src/main/java/com/api/show/image/infrastructure/ImageProdUploader.java
+++ backend/pcloud-api/src/main/java/com/api/show/image/infrastructure/ImageProdUploader.java
@@ -2,13 +2,21 @@ package com.api.show.image.infrastructure;
 
 import com.api.show.image.application.ImageUploader;
 import com.domain.show.common.image.domain.Image;
+import com.domain.show.common.image.exception.ImageException;
+import com.domain.show.common.image.exception.ImageExceptionType;
+import java.io.InputStream;
+import java.util.List;
+import java.util.stream.IntStream;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.util.List;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 @Slf4j
 @Profile("prod")
@@ -16,15 +24,53 @@ import java.util.List;
 @Component
 public class ImageProdUploader implements ImageUploader {
 
-    // TODO : S3 추가시 작업
+    private static final int START_INDEX = 0;
+    private static final String IMAGE_PREFIX = "images/";
+
+    @Value("${file.show.upload.location}")
+    private String location;
+
+    private final S3Client s3Client;
 
     @Override
     public void upload(final List<Image> convertedImages, final List<MultipartFile> originImageFiles) {
+        IntStream.range(START_INDEX, convertedImages.size())
+                .forEach(index -> uploadImage(convertedImages.get(index), originImageFiles.get(index)));
+    }
 
+    private void uploadImage(final Image image, final MultipartFile file) {
+        try (InputStream inputStream = file.getInputStream()) {
+            PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                    .bucket(location)
+                    .key(IMAGE_PREFIX + image.getUniqueName())
+                    .contentType(file.getContentType())
+                    .build();
+
+            s3Client.putObject(putObjectRequest, RequestBody.fromInputStream(inputStream, file.getSize()));
+            log.info("파일 저장 성공 : {}", image.getUniqueName());
+        } catch (Exception e) {
+            log.error("파일 저장 실패: {}, 로그 {} : ", image.getUniqueName(), e.getMessage());
+            throw new ImageException(ImageExceptionType.FILE_UPLOAD_FAILURE_EXCEPTION);
+        }
     }
 
     @Override
     public void deleteAll(final List<String> deletedImageUniqueNames) {
+        deletedImageUniqueNames.forEach(this::deleteImage);
+    }
 
+    private void deleteImage(final String imageName) {
+        try {
+            DeleteObjectRequest deleteObjectRequest = DeleteObjectRequest.builder()
+                    .bucket(location)
+                    .key(IMAGE_PREFIX + imageName)
+                    .build();
+
+            s3Client.deleteObject(deleteObjectRequest);
+            log.info("파일 삭제 성공 : {}", imageName);
+        } catch (Exception e) {
+            log.error("파일 삭제 실패: {}, 로그 {} : ", imageName, e.getMessage());
+            throw new ImageException(ImageExceptionType.FILE_DELETE_FAILURE_EXCEPTION);
+        }
     }
 }
